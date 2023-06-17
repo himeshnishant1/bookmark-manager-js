5921da02a7bb1dc4f019dc41d4161df0e5981bcf
himeshnishant1 <himeshnishant1@gmail.com>
2023-06-17 15:06:37 +0530

    Backend Finalized all working except theme;

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, query, where, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBvLoh-KsTHc-AsBtkBYalzxa5SET4RmA8",
@@ -18,35 +18,51 @@ const db = getFirestore();
 
 const colRef = collection(db, "bookmarks");
 
+let active_tag = "all";
+
 const addForm = document.querySelector(".add");
 addForm.addEventListener("submit", event => {
     event.preventDefault();
-    addDoc(colRef, {
-        link: addForm.link.value,
-        title: addForm.title.value,
-        category: addForm.category.value,
-        createdAt: serverTimestamp()
-    })
-    .then(() => {
-        addForm.reset();
-        fetchData();
-    });
-    
+    if (addForm.link.value !== "" && addForm.title.value !== "") {
+        addDoc(colRef, {
+            link: addForm.link.value,
+            title: addForm.title.value,
+            category: addForm.category.value,
+            createdAt: serverTimestamp()
+        })
+            .then(() => {
+                addForm.reset();
+                fetchData();
+            });
+    }
 });
 
 const cards = document.querySelector(".bookmark-cards");
-function fetchData(){
+
+// Fetch Data from firebase.
+
+function fetchData() {
     cards.innerHTML = "";
     getDocs(colRef)
-    .then(data => {
-        data.docs.forEach(document => {
-            let span = "";
-            if(document.data().category === "youtube") span = '<span class="card-tag active" style="background-color: rgb(153, 0, 0);color:white;">Youtube</span>';
-            else if(document.data().category === "netflix") span = '<span class="card-tag active" style="background-color: rgb(165, 11, 11);color:white;">Netflix</span>';
-            else if(document.data().category === "twitter") span = '<span class="card-tag active" style="background-color: rgb(3, 129, 247);color:white;">Twitter</span>';
-            else    span = '<span class="card-tag active" style="background-color: rgb(255, 174, 0);color:white;">Other</span>';
+        .then(data =>{
+            setCard(data);
+            setDeleteButtons();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
 
-            const card = `<div class="card">
+function setCard(data) {
+    cards.innerHTML = "";
+    data.docs.forEach(document => {
+        let span = "";
+        if (document.data().category === "youtube") span = '<span class="card-tag active" style="background-color: rgb(153, 0, 0);color:white;">Youtube</span>';
+        else if (document.data().category === "netflix") span = '<span class="card-tag active" style="background-color: rgb(165, 11, 11);color:white;">Netflix</span>';
+        else if (document.data().category === "twitter") span = '<span class="card-tag active" style="background-color: rgb(3, 129, 247);color:white;">Twitter</span>';
+        else span = '<span class="card-tag active" style="background-color: rgb(255, 174, 0);color:white;">Other</span>';
+
+        const card = `<div class="card">
                             <div class="card-header">
                                 <p class="card-title">${document.data().title}</p>
                             </div>
@@ -57,18 +73,68 @@ function fetchData(){
                                 <div class="functions">
                                     <i class="bi bi-box-arrow-up-right" style="color:#000000" onclick="window.open('${document.data().link}')"></i>
                                     <i class="bi bi-google" style="color:#1b6ae9" onclick="window.open('https://www.google.com/search?q=${document.data().title}')"></i>
-                                    <i class="bi bi-trash" style="color:#f33838" data-id="${document.id}"></i>
+                                    <i class="bi bi-trash delete" style="color:#f33838" data-id="${document.id}"></i>
                                 </div>
                             </div>
                         </div>`;
-            
-            cards.innerHTML += card;
-            
-        });
-    })
-    .catch(error => {
-        console.log(error);
+
+        cards.innerHTML += card;
+
     });
 }
 
 fetchData();
+
+
+//Delete Data
+function setDeleteButtons() {
+    const deleteButtons = document.querySelectorAll("i.delete");
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', event => {
+            const deleteRef = doc(db, "bookmarks", button.dataset.id);
+            deleteDoc(deleteRef)
+                .then(() => {
+                    button.parentElement.parentElement.parentElement.remove();
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        });
+    });
+}
+
+const categoryList = document.querySelector(".tags");
+categoryList.addEventListener('click', event => {
+    if (event.target.tagName === "SPAN") {
+
+        const children = categoryList.children;
+
+        for (let i = 0; i < children.length; i++) {
+
+            const child = children[i];
+
+            if (child.classList[0] === active_tag) {
+                child.classList.remove("active");
+                active_tag = event.target.classList[0];
+                event.target.classList.add("active");
+            }
+        }
+
+        if (event.target.classList[0] === "all") {
+            fetchData();
+        }
+        else {
+            const qRef = query(colRef, where("category", "==", event.target.classList[0]));
+            getDocs(qRef)
+                .then(data =>{
+                    setCard(data);
+                    setDeleteButtons();
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
+});
+
+
